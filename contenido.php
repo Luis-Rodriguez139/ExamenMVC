331209bf718d8b914ba90c814605e4318fa60616
unknown <lsenrique139@gmail.com>
2020-10-01 20:58:50 -0500

    primer commit

---
+++ contenido.php
@@ -0,0 +1,50 @@
+<?php 
+
+	function contenido(){
+		$datos=array();
+
+		$datos[0]="imgperros/bu3.png" . "||".
+				  "Bulldog"."||".
+				  "Es una raza canina originaria del Reino Unido. Su ancestro, conocido como el Antiguo Bulldog Inglés, fue utilizado en peleas de perros con toros, hasta mediados del siglo XVII, aunque en 1835 esta práctica fue prohibida en Reino Unido.6 En la actualidad este perro es uno de los símbolos de Inglaterra.";
+
+
+
+		$datos[1]="imgperros/la3.jpg".  "||".
+				  "Labrador"."||".
+				  "Es una raza canina originaria de Terranova, en la actual Canadá. Es una de las razas más populares del mundo por la cantidad de ejemplares registrados. Su excepcional afabilidad, gentileza, inteligencia, nobleza, energía y bondad, hacen que los labradores sean generalmente considerados como buenos compañeros para personas de todas las edades, así como fiables perros trabajadores, comúnmente formando parte de las brigadas caninas de la policía en operativos antidroga, antiexplosivos, de búsqueda y rescate, entre otros. Con adiestramiento, el labrador es una de las razas de perro más dócil, obediente y talentosa que existen.";
+
+
+		$datos[2]="imgperros/san1.jpg" ."||".
+				  "San Bernardo"."||".
+				  "Es una raza de perro originaria de los Alpes suizos y del norte de Italia. Es el perro boyero más famoso junto con el mastín del Pirineo y es muy próximo al gran boyero suizo. Desciende del extinto mastín de los Alpes, que se mezcló con otras razas como el dogo del Tíbet, el terranova y el gran danés. ";
+
+		$datos[3]="imgperros/bul2.jpg" ."||".
+				  "Bull Terry"."||".
+				  " Es una raza canina de la familia de los terrier. Son conocidos por la forma única de su cabeza y sus pequeños ojos en forma triangular. Su temperamento ha sido descrito como amante de la diversión, así como activos y graciosos. Han aparecido como personajes en tiras cómicas, libros, películas y publicidad.";
+
+		$datos[4]="imgperros/ca1.jpg" ."||".
+				  "Caniche"."||".
+				  "Es una raza canina que hasta el siglo XV se consideró de uso exclusivo de los aristócratas y nobles. Fue un perro cobrador de aguas hasta el Renacimiento: recuperaban las presas ya cazadas que habían caído al agua, como patos y cisnes. Hoy en día se les encuentra frecuentemente en las exposiciones caninas de belleza. Existen cuatro variedades: grande, mediano, enano y toy. Aparte del caniche común, también existe la variedad Cordelé. ";
+		
+		$datos[5]="imgperros/sa1.jpg" ."||".
+				  "Dachshund"."||".
+				  "Es una raza canina. Su peculiar fisonomía se debe a una mutación genética conocida como bassetismo, que dota a los ejemplares de unas extremidades cortas en relación con el tamaño del cuerpo. Según su tamaño y peso puede clasificarse en estándar (9kg-11kg), miniatura (4,5kg-6kg).  Además el dachshund se diferencia en función de la tipología del pelo, puede ser de pelo duro (generalmente de color gris), pelo corto y pelo largo, estos dos últimos pueden ser de color negro con rojo fuego, marrón chocolate y dorado. ";
+		
+
+		$datos[6]="imgperros/ps4.jpg"  ."||".
+				  "Pastor Aleman"."||".
+				  "Es relativamente nueva, forman parte del grupo de pastoreo, debido a que fueron perros desarrollados originalmente para reunir y vigilar ovejas. Desde entonces, sin embargo, gracias a su fuerza, inteligencia,  capacidad de entrenamiento y obediencia, los pastores alemanes de todo el mundo son a menudo la raza preferida para muchos otros tipos de trabajo, como son: Perro policía, perro guardián, guía de ciegos, animal de rescate, y otros, según el uso que le den las fuerzas y cuerpos de seguridad y el ejército.";
+
+
+		$datos[7]="imgperros/cho4.jpg" ."||".
+				  "Chow chow"."||".
+				  "Es una raza canina originaria del Norte de China. Se cree que  fue uno de los perros nativos utilizados como modelo para el perro de Foo, los guardianes tradicionales de piedra que se encuentran en frente de los templos y palacios budistas. Es una de las pocas razas de perros antiguas que todavía existen hoy en día en el mundo.";
+
+
+		$datos[8]="imgperros/hu1.jpg" ."||".
+				  "Husky Siberiano"."||".
+				  "Es una raza de perro de trabajo originaria del noreste de Siberia (Chukotka, Rusia). Esta raza presenta un acusado parecido con el lobo. Originalmente fue criado por la tribu chukchi, en la que se utilizaba como perro de compañía para pastorear a los ciervos, tirar de los trineos y mantener calientes a los niños; en la actualidad se encuentra en diversas zonas del mundo.";
+		return $datos;
+	}
+
+ ?>
